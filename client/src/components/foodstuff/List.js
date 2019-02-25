653c86a98fdc6a2c937bb499bb106ad26484382f
sebcnst <sebcnstt@gmail.com>
2019-02-25 13:29:22 +0100

    [CLIENT] Fix modal issue merce la famille

--- client/src/components/foodstuff/List.js
+++ client/src/components/foodstuff/List.js
@@ -7,6 +7,7 @@ import Create from './Create';
 import logo from './assets/img/logo-emy.png';
 import Modal from 'react-awesome-modal';
 import MapContainer from './map/MapContainer';
+import Item from './list/ProductList';
 import './App.css';
 
 
@@ -156,46 +157,7 @@ class List extends Component {
             {this.props.retrieved &&
               this.props.retrieved['hydra:member'].map(item => (
 
-              <li key={item['@id']}>
-
-                <img src={require('./assets/img/1.jpg')} className="img-produit" alt=""/>
-
-              <div className="ctn-desc-item">
-                  <h2>{item['name']}</h2>
-                  
-                  <span>
-                  <img src={require('./assets/img/calendar.png')} className="img-calendar" alt=""/>
-                      DDP : <span>{item['expirationDate']}</span>
-                  </span>
-
-                  <a className="take-it-infos" value="Open" onClick={() => this.openModal()}>Je prends !</a>
-                  <a href="">
-                  <img src={require('./assets/img/place-localizer.png')} className="img-calendar" alt=""/>
-                  Localiser
-                  </a>
-              </div>
-
-                <Modal visible={this.state.visible} width="400" className="modal-popup modal-popup2" effect="fadeInUp" onClickAway={() => this.closeModal()}>
-                    <div className="popup-takeit">
-                        <img src={require('./assets/img/close.png')} className="close-popup" alt="Fermer la popup" onClick={() => this.closeModal()}/>
-
-                        <h3>Planifier le rendez-vous !</h3>
-
-                        <div className="side-product-popup">
-                        <img src={require('./assets/img/1.jpg')} className="img-produit" alt=""/>
-                          <span>
-                            <h4>{item['name']}</h4>
-                            <p>
-                                <img src={require('./assets/img/calendar.png')} className="img-calendar" alt=""/>
-                                DDP : <span>{item['expirationDate']}</span>
-                            </p>
-                            <button type="button" name="button">{item['phoneNumber']}</button>
-                          </span>
-                        </div>
-
-                    </div>
-                </Modal>
-              </li>
+              <Item item={item} key={item.id}/>
 
               ))}
               </ul>
